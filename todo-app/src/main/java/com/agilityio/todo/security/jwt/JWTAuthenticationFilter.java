2a4e2bfd98f147b3b15643d47cdf1f8c7b0fc0db
Thien Le Long <thien.lelong@asnet.com.vn>
2017-02-08 14:08:06 +0700

    Validate Expired Date authentication

--- todo-app/src/main/java/com/agilityio/todo/security/jwt/JWTAuthenticationFilter.java
+++ todo-app/src/main/java/com/agilityio/todo/security/jwt/JWTAuthenticationFilter.java
@@ -1,26 +1,31 @@
 package com.agilityio.todo.security.jwt;
 
+import com.agilityio.todo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
  * Project: toto-app
  * Authentication Filter Jwt
  */
-public class JWTAuthenticationFilter extends GenericFilterBean {
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        Authentication authentication = new TokenAuthenticationService().getAuthentication((HttpServletRequest) servletRequest);
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        Authentication authentication = new TokenAuthenticationService().getAuthentication(request);
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        filterChain.doFilter(servletRequest, servletResponse);
+        chain.doFilter(request, response);
     }
 }
