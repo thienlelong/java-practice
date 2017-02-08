83b7425eebccac99890b63c85024a9fa751a57be
Thien Le Long <thien.lelong@asnet.com.vn>
2017-02-08 08:30:09 +0700

    Add GlobalAuthenticationConfigure for user authen

---
+++ todo-app/src/main/java/com/agilityio/todo/config/GlobalAuthenticationConfiguration.java
@@ -0,0 +1,58 @@
+package com.agilityio.todo.config;
+
+import com.agilityio.todo.domain.User;
+import com.agilityio.todo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author thienlelong
+ */
+@Configuration
+@EnableGlobalAuthentication
+public class GlobalAuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                User user = userService.findUsersByUserName(username);
+                if (user != null) {
+                    return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(),
+                            true, true, true, true, AuthorityUtils.createAuthorityList("ROLE_USER"));
+                } else {
+                    throw new UsernameNotFoundException("Could not find the user'" + username + "'");
+                }
+            }
+
+        };
+    }
+
+    @Bean(name = "passwordEncoder")
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
