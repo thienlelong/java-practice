bcb10755fcb58ba1a95eef842011032951b360a1
Thien Le Long <thien.lelong@asnet.com.vn>
2017-02-10 16:23:31 +0700

    Add new banner for every environment

--- todo-app/src/main/java/com/agilityio/todo/TodoAppApplication.java
+++ todo-app/src/main/java/com/agilityio/todo/TodoAppApplication.java
@@ -1,12 +1,24 @@
 package com.agilityio.todo;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 public class TodoAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoAppApplication.class, args);
+		new SpringApplicationBuilder(TodoAppApplication.class)
+				.banner(new Banner() {
+					@Override
+					public void printBanner(Environment environment, Class<?> sourceClass,
+											PrintStream out) {
+						out.print("\n\t Todo App".toUpperCase());
+					}
+				})
+				.run(args);
 	}
 }
